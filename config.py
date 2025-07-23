ded1824f94521fad03135994624bc139c4751885
shayanatlog <bot587107@gmail.com>
2025-07-23 21:59:57 +0330

    Update config.py

--- config.py
+++ config.py
@@ -11,3 +11,7 @@ AUTHORIZED_USERS = ["Robot.NM", "389._.20"]
 SPAM_COUNT = 100
 DANCE_REPEAT_DELAY = 2.0  # seconds between dance repeats
 SPAM_DELAY = 0.5  # seconds between spam messages
+
+HIGHRISE_ROOM_ID = "687d5f34b342b9609d827132"
+
+HIGHRISE_BOT_TOKEN = "68495405d4fb68f4a423ff092cc4157f5a11b95bdbd8e1fd989b3602a56ea13c"
